149f951903840e01556f0eaac8f7f278a71dad4c
azharie <azharie@staff.gramedia.com>
2018-07-30 00:02:31 +0700

    handle input new currency

--- src/app/components/CurrencyTable.tsx
+++ src/app/components/CurrencyTable.tsx
@@ -13,10 +13,12 @@ interface IState {
   baseNum: number
   initialValue: number
   defaultCurrency: Array<string>
+  moreCurrencies: Array<string>
   exchange: number
   base: string
   date: string
-  rates: any
+  rates: any,
+  newCurrency: string
 }
 
 export class CurrencyTable extends React.Component<{}, IState> {
@@ -26,14 +28,28 @@ export class CurrencyTable extends React.Component<{}, IState> {
       baseNum: 10000,
       initialValue: 0,
       defaultCurrency: ['EUR', 'GBP', 'SGD'],
+      moreCurrencies: [
+        'CAD',
+        'IDR',
+        'GBP',
+        'CHF',
+        'INR',
+        'MYR',
+        'JPY',
+        'KRW'
+      ],
       exchange: 0,
       base: '',
       date: '',
-      rates: {}
+      rates: {},
+      newCurrency: ''
       
     }
     this.getExchangeRate = this.getExchangeRate.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputNewCurrency = this.handleInputNewCurrency.bind(this)
+    this.addNewcurrency = this.addNewcurrency.bind(this)
   }
 
   public componentDidMount() {
@@ -51,7 +67,8 @@ export class CurrencyTable extends React.Component<{}, IState> {
   shouldComponentUpdate(nextProps: any, nextState: any) {
     let shouldUpdate1 = this.state.rates !== nextState.rates;
     let shouldUpdate2 = this.state.baseNum !== nextState.baseNum;
-    return shouldUpdate1 || shouldUpdate2
+    let shouldUpdate3 = this.state.defaultCurrency !== nextState.defaultCurrency
+    return shouldUpdate1 || shouldUpdate2 ||  shouldUpdate3
   }
 
   public getExchangeRate (rate: any): any {
@@ -70,7 +87,27 @@ export class CurrencyTable extends React.Component<{}, IState> {
       baseNum: event.target.value
     })
   }
-  
+
+  public handleInputNewCurrency(event: any): any {
+    this.setState({
+      newCurrency: event.target.value
+    })
+  }
+
+  public addNewcurrency() {
+    this.setState({
+      defaultCurrency: [...this.state.defaultCurrency, this.state.newCurrency],
+    })
+    // console.log([...this.state.defaultCurrency, this.state.newCurrency])
+    console.log(this.state.moreCurrencies);
+    
+  }
+
+  public handleSubmit(event: any): any {
+    console.log(this.state.newCurrency)
+    this.addNewcurrency()
+    event.preventDefault()
+  }
   public render() {
     console.log(this.state)
     return (
@@ -90,6 +127,15 @@ export class CurrencyTable extends React.Component<{}, IState> {
             )}
           </Card.Content>
         </Card.Content>
+        <form onSubmit={this.handleSubmit}>
+          <input list="browsers" name="myBrowser" onChange={(e) => this.handleInputNewCurrency(e)} />
+            <datalist id="browsers">
+            {this.state.moreCurrencies.map((item: string, idx:number) => 
+                <option key={idx} value={item} />
+            )}
+            </datalist>
+            <input type="submit" value="+" />
+        </form>
       </Card>
       </Card.Group>
     )
